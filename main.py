a01a79173cdd440eb57915114401f009949e870b
Nikita Kuznetsov <esperoproduction@gmail.com>
2018-05-02 16:09:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 import webbrowser
-import networkx as nx
-from networkx.algorithms import isomorphism
-import matplotlib.pyplot as plt
 import analysis_function as af
 import substructure_algo as sub_algo
 import graph_function as gf
@@ -22,11 +19,11 @@ def upload_file():
     if coggle_user.access_token == "":
         coggle_user.authorization_token()
     if request.method == 'POST':
-        if 'file' not in request.files:
+        if 'file_input' not in request.files:
             flask.flash('No file part')
             return redirect(request.url)
 
-        file = request.files['file']
+        file = request.files['file_input']
         form = request.form['form_algo']
 
         if file.filename == '':
@@ -42,15 +39,25 @@ def upload_file():
             return redirect(request.url)
     return '''
     <!doctype html>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+    <font face="Source Sans Pro" >
     <title>Анализ интеллект карт</title>
     <h1>Анализ интеллект карт</h1>
     <form method=post enctype=multipart/form-data>
         <input type=radio id="contactChoice1" name="form_algo" value="without_ref" checked>
-                <label for="contactChoice1">Сравнение двух интеллект карт</label>
-            <input type=radio id="contactChoice2" name="form_algo" value="with_ref">
-                <label for="contactChoice2">Сравнение карт с референцными</label>
+            <label for="contactChoice1">Сравнение двух интеллект карт с референцными</label>
         <p>
-        <input type=file name=file>
+        <input type=radio id="contactChoice2" name="form_algo" value="with_ref">
+            <label for="contactChoice2">Сравнение карт с референцными</label>
+        <p>
+        <label for="file">Загрузка файла с входными данными</label>
+        <p>
+        <input type=file name=file_input>
+        <input type=submit value=Загрузить>
+        <p>
+        <label for="file">Загрузка файла для выходных данных</label>
+        <p>
+        <input type=file name=file_output>
         <input type=submit value=Загрузить>
     </form>
     '''
@@ -62,8 +69,41 @@ def take_mm(filename, status = None):
     id_diagram_1 = sheet['A1'].value
     id_diagram_2 = sheet['A2'].value
     if status is None:
-        mind_map_1 = information_for_algo(id_diagram_1)
-        mind_map_2 = information_for_algo(id_diagram_2)
+        arr = []
+        arr.append(information_for_algo(id_diagram_1))
+        arr.append(information_for_algo(id_diagram_2))
+        arr_ids = gf.read_mm_ids(sheet, "B")
+        n = len(arr_ids)
+        mass = [[0] * 2 for i in range(n)]
+
+        for i in range(0, len(arr_ids)):
+            for j in range(0, 2):
+                curr_map = information_for_algo(arr_ids[i])
+                new_graph = copy.deepcopy(arr[j]['graph'])
+                algo = sub_algo.max_comp_element(arr[j]['diagram'], new_graph, curr_map['graph'])
+                last_graph = gf.create_graph_form_list(algo)
+                mass[i][j] = af.similarity_sub_algo(last_graph, arr[j]['graph'], curr_map['graph'])
+        output_sheet = wb.create_sheet('matrix')
+        gf.print_matrix(output_sheet, mass)
+        wb.save(filename)
+    else:
+        arr_ids = gf.read_mm_ids(sheet, "B")
+        n = len(arr_ids)
+        mass = [[0] * n for i in range(n)]
+        for i in range(0, len(arr_ids)):
+            for j in range(0, len(arr_ids)):
+                mind_map_1 = information_for_algo(arr_ids[i])
+                mind_map_2 = information_for_algo(arr_ids[j])
+                new_graph_1 = copy.deepcopy(mind_map_1['graph'])
+                algo = sub_algo.max_comp_element(mind_map_1['diagram'], new_graph_1, mind_map_2['graph'])
+                last_graph = gf.create_graph_form_list(algo)
+                mass[i][j] = af.similarity_sub_algo(last_graph, mind_map_1['graph'], mind_map_2['graph'])
+        output_sheet = wb.create_sheet('matrix')
+        gf.print_matrix(output_sheet, mass)
+        wb.save(filename)
+'''
+# mind_map_1 = information_for_algo(id_diagram_1)
+        # mind_map_2 = information_for_algo(id_diagram_2)
         new_graph_1 = copy.deepcopy(mind_map_1['graph'])
 
         algo = sub_algo.max_comp_element(mind_map_1['diagram'], new_graph_1, mind_map_2['graph'])
@@ -75,22 +115,7 @@ def take_mm(filename, status = None):
         output_sheet['A2'] = procent
         gf.print_metrics(output_sheet, af.metrics(mind_map_1['diagram'], mind_map_1['graph']), af.metrics(mind_map_2['diagram'], mind_map_2['graph']))
         wb.save(filename)
-    else:
-        arr_ids = gf.read_mm_ids(sheet, "B")
-        n = len(arr_ids)
-        mass = [[0] * n for i in range(n)]
-        for i in range(0, len(arr_ids) - 1):
-            for j in range(0, len(arr_ids) - 1):
-                mind_map_1 = information_for_algo(arr_ids[i])
-                mind_map_2 = information_for_algo(arr_ids[j])
-                new_graph_1 = copy.deepcopy(mind_map_1['graph'])
-
-                algo = sub_algo.max_comp_element(mind_map_1['diagram'], new_graph_1, mind_map_2['graph'])
-                last_graph = gf.create_graph_form_list(algo)
-                mass[i][j] = af.similarity_sub_algo(last_graph, mind_map_1['graph'], mind_map_2['graph'])
-        output_sheet = wb.create_sheet('matrix')
-        gf.print_matrix(output_sheet, mass, len(arr_ids))
-        wb.save(filename)
+'''
 
 
 def information_for_algo(id_diagram):
