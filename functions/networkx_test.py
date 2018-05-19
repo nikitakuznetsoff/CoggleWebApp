9240729a9a6801fc9dea9aeb0e21e62edc24d834
Nikita Kuznetsov <esperoproduction@gmail.com>
2018-05-19 21:40:35 +0300

    Add files via upload

---
+++ functions/networkx_test.py
@@ -0,0 +1,134 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.algorithms import isomorphism
+
+Graph = nx.DiGraph()
+Graph.add_nodes_from([1, 2, 3, 4])
+Graph.add_edges_from([(1, 2), (1, 3), (4, 1)])
+print(list(Graph.neighbors(1)))
+print(list(Graph.successors(1)))
+print("...")
+
+G = nx.Graph()
+# Вершины
+G.add_node(1)
+# Добавление вершин из списка
+G.add_nodes_from([2, 3])
+# Пучок вершин
+H = nx.path_graph(10)
+# Добавить вершины из H в G
+G.add_nodes_from(H)
+# Использовать граф H, как вершину в G
+G.add_node(H)
+
+# Ребра
+G.add_edge(1, 2)
+e = (1, 2)
+G.add_edge(*e)
+# add from list
+G.add_edges_from([(1, 2), (1, 3)])
+G.add_edges_from(H.edges())
+
+# delete
+# G.remove_node(H)
+# G.remove_nodes_from(H)
+# G.remove_edge(H)
+# G.remove_edges_from(H)
+
+###
+G.clear()
+G.add_edges_from([(1, 2), (1, 3)])
+G.add_node(4)
+G.add_node("spam")
+G.add_nodes_from("spam")
+print(G.number_of_edges())
+print(G.number_of_nodes())
+
+print(G.nodes())
+print(G.edges())
+print("tut")
+print(list(G.neighbors('spam')))
+print("tut")
+
+print()
+G.remove_nodes_from("spam")
+print(G.nodes())
+G.remove_edge(1, 3)
+print(G.edges())
+
+print()
+H = nx.DiGraph(G)
+print(H.edges())
+H.clear()
+edgelist = [(0, 1), (1, 2), (2, 3)]
+H = nx.DiGraph(edgelist)
+print(H.edges())
+
+G[1][2]['color'] = 'blue'
+print(G[1][2])
+G[1][2]['color'] = 'red'
+print(G[1][2])
+
+###
+FG = nx.Graph()
+FG.add_weighted_edges_from([(1, 2, 0.4), (1, 3, 0.4), (2, 3, 0.4)])
+print()
+for (a, b, c) in FG.edges(data='weight'):
+    if c < 0.5: print('(%d, %d, %.3f)' % (a, b, c))
+
+###
+print()
+G.clear()
+G = nx.Graph(day="Friday")
+print(G.graph)
+
+G.add_node(1, time='5pm')
+print(G.node[1])
+G.node[1]['room'] = 700
+print('loh')
+print(G.nodes[1])
+print()
+
+G.add_edge(1, 2, weight=4.5)
+print(G[1][2])
+G.add_edges_from([(3, 4), (4, 5)], color='red')
+print(G[3][4], G[4][5])
+G.add_edges_from([(1, 2, {'color': 'blue'}), (2, 3, {'weight': 2.2})])
+print()
+print(G[1][2], G[2][3])
+
+###
+print()
+DG = nx.DiGraph()
+DG.add_weighted_edges_from([(1, 2, 0.5), (3, 1, 0.2)])
+print(DG.out_degree(1, weight='weight'))
+print(DG.degree(1, weight='weight'))
+DG.successors(1)
+print("qwe")
+print(list(DG.neighbors(1)))
+print("...")
+
+#M = nx.read_graphml("file2.graphml")
+#nx.draw(M)
+#plt.show()
+#print(M.edges())
+#print(M.nodes(data=True))
+#print(M.node['1aa42b381ab47751db9a6a78ce2a72ea'])
+
+'''
+Z = nx.parse_adjlist('start.graphml')
+nx.draw(Z)
+plt.show(Z)
+'''
+
+A = nx.Graph()
+B = nx.Graph()
+
+A.add_nodes_from([1, 2])
+B.add_nodes_from([3, 4])
+
+A.add_edge(1, 2)
+B.add_edge(3, 4)
+
+GM = isomorphism.GraphMatcher(A, B)
+print(GM.is_isomorphic())
--- functions/subtree_isomorphism.py
+++ functions/subtree_isomorphism.py
@@ -5,6 +5,51 @@ from functions import graph_function as gf
 from functions import substructure_algo as sa
 import copy
 
+# Делаем поддерево от заданной точки
+def subtree_by_node(graph, curr_node):
+    new_graph = nx.DiGraph()
+    new_graph.add_node(curr_node)
+    for node in graph.neighbors(curr_node):
+        new_graph.add_node(node)
+        new_graph.add_edge(curr_node, node)
+        subtree_by_node_rec(graph, node, new_graph)
+    return new_graph
+
+def subtree_by_node_rec(graph, curr_node, new_graph):
+    for node in graph.neighbors(curr_node):
+        new_graph.add_node(node)
+        new_graph.add_edge(curr_node, node)
+        subtree_by_node_rec(graph, node, new_graph)
+
+def max_common_substree(G, H):
+    return 0
+
+
+# Основной алгоритм
+def multiplicity_subtree(G, H):
+    G_graph = gf.transform_into_graph(G)
+    H_graph = gf.transform_into_graph(H)
+    nodes_of_G = G_graph.nodes()
+    nodes_of_G.remove(G['_id'])
+
+    D = []
+    for node_G in nodes_of_G:
+        for node_H in H_graph.nodes():
+            arr_of_node = list(H_graph.neighbors(node_H))
+            arr_of_node.append(node_H)
+            for s in arr_of_node:
+                arr = []
+                for u in G_graph.neighbors(node_G):
+                    arr.append([])
+                    i = 0
+                    for v in H_graph.neighbors(node_H):
+                        node_substree_G = subtree_by_node(G, u)
+                        node_substree_H = subtree_by_node(H, v)
+                        summ = 1 + max_common_substree(node_substree_G, node_substree_H)
+                        arr[i].append(summ)
+                        i += 1
+
+'''
 def multiplicity_subtree(G, H):
     # Массивы поддеревьев
     subtrees_G = []
@@ -27,5 +72,8 @@ def multiplicity_subtree(G, H):
 
     for vertex_G in subtrees_G:
         for vertex_H in subtrees_H:
-            
+
+'''
+
+
 
