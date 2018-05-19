f9d0ef43387656d2e74d7894624d09be3eecbac8
Nikita Kuznetsov <esperoproduction@gmail.com>
2018-05-20 00:38:23 +0300

    Add files via upload

--- functions/subtree_isomorphism.py
+++ functions/subtree_isomorphism.py
@@ -15,13 +15,40 @@ def subtree_by_node(graph, curr_node):
         subtree_by_node_rec(graph, node, new_graph)
     return new_graph
 
+
 def subtree_by_node_rec(graph, curr_node, new_graph):
     for node in graph.neighbors(curr_node):
         new_graph.add_node(node)
         new_graph.add_edge(curr_node, node)
         subtree_by_node_rec(graph, node, new_graph)
 
+
+# Создание графа от заднного корня до конкретной вершины
+def make_graph(graph, root, node):
+    list_nodes = nx.shortest_path(graph, source=root, target=node)
+    new_graph = nx.DiGraph()
+    new_graph.add_nodes_from(list_nodes)
+    curr_node = graph[graph.node[root['_id']]]
+    for obj in new_graph.nodes():
+        if obj == curr_node:
+            continue
+        # obj = graph[obj]
+        new_graph.add_edge(curr_node, obj)
+        curr_node = obj
+        # new_graph[curr_node][obj]['colour'] = graph[curr_node][obj]['colour']
+    return new_graph
+
+
+# Поиск максимального весового сравнения (MWM) в заданной матрице
+def max_mwm(arr):
+
+    return 0
+
+
+# Поиск максимального общего дерева
 def max_common_substree(G, H):
+    gm_one = isomorphism.DiGraphMatcher(G, H)
+    gm_two = isomorphism.DiGraphMatcher(H, G)
     return 0
 
 
@@ -29,10 +56,11 @@ def max_common_substree(G, H):
 def multiplicity_subtree(G, H):
     G_graph = gf.transform_into_graph(G)
     H_graph = gf.transform_into_graph(H)
+
     nodes_of_G = G_graph.nodes()
     nodes_of_G.remove(G['_id'])
-
     D = []
+
     for node_G in nodes_of_G:
         for node_H in H_graph.nodes():
             arr_of_node = list(H_graph.neighbors(node_H))
@@ -40,15 +68,20 @@ def multiplicity_subtree(G, H):
             for s in arr_of_node:
                 arr = []
                 for u in G_graph.neighbors(node_G):
+                    graph_form_root_G = make_graph(G_graph, node_G, u)
+                    node_substree_G = subtree_by_node(G_graph, u)
                     arr.append([])
                     i = 0
                     for v in H_graph.neighbors(node_H):
-                        node_substree_G = subtree_by_node(G, u)
-                        node_substree_H = subtree_by_node(H, v)
-                        summ = 1 + max_common_substree(node_substree_G, node_substree_H)
+                        graph_form_root_H = make_graph(H_graph, node_H, v)
+                        node_substree_H = subtree_by_node(H_graph, v)
+                        summ = max_common_substree(graph_form_root_G, graph_form_root_H) \
+                               + max_common_substree(node_substree_G, node_substree_H)
                         arr[i].append(summ)
                         i += 1
-
+                graph_form_root_r = make_graph(G_graph, G_graph[G['_id']], node_G)
+                graph_form_root_s = make_graph(H_graph, node_H, s)
+                D.append(max_common_substree(graph_form_root_r, graph_form_root_s) + max_mwm(arr))
 '''
 def multiplicity_subtree(G, H):
     # Массивы поддеревьев
