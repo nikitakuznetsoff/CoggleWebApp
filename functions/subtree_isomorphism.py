72312b035201a93e795172718418d0d8a66fe70b
Nikita Kuznetsov <esperoproduction@gmail.com>
2018-05-20 21:38:55 +0300

    Add files via upload

--- functions/subtree_isomorphism.py
+++ functions/subtree_isomorphism.py
@@ -24,31 +24,53 @@ def subtree_by_node_rec(graph, curr_node, new_graph):
 
 
 # Создание графа от заднного корня до конкретной вершины
-def make_graph(graph, root, node):
-    list_nodes = nx.shortest_path(graph, source=root, target=node)
+def make_graph(graph, root, nd):
+    list_nodes = nx.shortest_path(graph, source=root, target=nd)
     new_graph = nx.DiGraph()
     new_graph.add_nodes_from(list_nodes)
-    curr_node = graph[graph.node[root['_id']]]
-    for obj in new_graph.nodes():
+
+    curr_node = list_nodes[list_nodes.index(root)]
+    for obj in list_nodes:
         if obj == curr_node:
+            # new_graph.node[obj]['text'] = graph.node[obj]['text']
             continue
-        # obj = graph[obj]
         new_graph.add_edge(curr_node, obj)
         curr_node = obj
+        # obj = присваиваем метрики каждой вершине графа new_graph аналошичных вершин из graph
         # new_graph[curr_node][obj]['colour'] = graph[curr_node][obj]['colour']
+        # new_graph.node[obj]['text'] = graph.node[obj]['text']
     return new_graph
 
 
 # Поиск максимального весового сравнения (MWM) в заданной матрице
 def max_mwm(arr):
-
-    return 0
+    summ = 0
+    if len(arr) == 0:
+        return 0
+    if len(arr) < len(arr[0]):
+        for i in range(len(arr)):
+            summ += max(arr[i])
+    else:
+        for i in range(len(arr[0])):
+            index = []
+            for j in range(len(arr)):
+                index.append(arr[j][i])
+            summ += max(index)
+    return summ
 
 
 # Поиск максимального общего дерева
 def max_common_substree(G, H):
     gm_one = isomorphism.DiGraphMatcher(G, H)
-    gm_two = isomorphism.DiGraphMatcher(H, G)
+   # gm_two = isomorphism.DiGraphMatcher(H, G)
+    #em = isomorphism.categorical_edge_match('colour', '')
+    #nm = isomorphism.categorical_node_match('text', '')
+    #GM = isomorphism.DiGraphMatcher(comp_graph, curr_graph, node_match=nm, edge_match=em)
+    qwe = gm_one.subgraph_is_isomorphic()
+    if gm_one.subgraph_is_isomorphic():
+        return af.count_nodes(H)
+    #elif gm_two.subgraph_is_isomorphic():
+    #    return af.count_nodes(G)
     return 0
 
 
@@ -56,9 +78,8 @@ def max_common_substree(G, H):
 def multiplicity_subtree(G, H):
     G_graph = gf.transform_into_graph(G)
     H_graph = gf.transform_into_graph(H)
-
-    nodes_of_G = G_graph.nodes()
-    nodes_of_G.remove(G['_id'])
+    nodes_of_G = list(G_graph.nodes())
+    nodes_of_G.remove(G[0]['_id'])
     D = []
 
     for node_G in nodes_of_G:
@@ -67,21 +88,23 @@ def multiplicity_subtree(G, H):
             arr_of_node.append(node_H)
             for s in arr_of_node:
                 arr = []
+                i = 0
                 for u in G_graph.neighbors(node_G):
                     graph_form_root_G = make_graph(G_graph, node_G, u)
                     node_substree_G = subtree_by_node(G_graph, u)
                     arr.append([])
-                    i = 0
                     for v in H_graph.neighbors(node_H):
                         graph_form_root_H = make_graph(H_graph, node_H, v)
                         node_substree_H = subtree_by_node(H_graph, v)
                         summ = max_common_substree(graph_form_root_G, graph_form_root_H) \
                                + max_common_substree(node_substree_G, node_substree_H)
                         arr[i].append(summ)
-                        i += 1
-                graph_form_root_r = make_graph(G_graph, G_graph[G['_id']], node_G)
+                    i += 1
+                graph_form_root_r = make_graph(G_graph, G[0]['_id'], node_G)
                 graph_form_root_s = make_graph(H_graph, node_H, s)
                 D.append(max_common_substree(graph_form_root_r, graph_form_root_s) + max_mwm(arr))
+    return max(D)
+
 '''
 def multiplicity_subtree(G, H):
     # Массивы поддеревьев
