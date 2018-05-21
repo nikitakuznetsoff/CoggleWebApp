993ca8fdddf446a6621d84f8534ea604a353468a
Nikita Kuznetsov <esperoproduction@gmail.com>
2018-05-21 21:40:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 import webbrowser
-from functions import analysis_function as af
-from functions import substructure_algo as sub_algo
-from functions import graph_function as gf
 import copy
 import flask
 from openpyxl import load_workbook
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 from werkzeug.utils import secure_filename
+from functions import analysis_function as af
+from functions import substructure_algo as sub_algo
+from functions import graph_function as gf
+from functions import subtree_isomorphism as si
+
 
 app = Flask(__name__)
-
+app.secret_key = 'some_secret'
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -20,15 +22,20 @@ def upload_file():
         coggle_user.authorization_token()
     if request.method == 'POST':
         if 'file_input' not in request.files:
-            flask.flash('No file part')
+            flask.flash('No file part', 'input')
             return redirect(request.url)
 
         file = request.files['file_input']
+        #file_ref = request.files['files_input_ref']
         form = request.form['form_algo']
 
         if file.filename == '':
-            flask.flash('No selected file')
+            flask.flash('No selected file', 'input')
             return redirect(request.url)
+        #if file_ref.filename == '':
+        #    flask.flash('Вы не выбрали файл!', 'input_ref')
+        #    return redirect(request.url)
+
         if file:
             filename_file = secure_filename(file.filename)
             file.save(filename_file)
@@ -37,27 +44,7 @@ def upload_file():
             elif form == "with_ref":
                 take_mm(filename_file, True)
             return redirect(request.url)
-    return '''
-    <!doctype html>
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
-    <font face="Source Sans Pro" >
-    <title>Анализ интеллект карт</title>
-    <h1>Анализ интеллект карт</h1>
-    <form method=post enctype=multipart/form-data>
-        <input type=radio id="contactChoice1" name="form_algo" value="without_ref" checked>
-            <label for="contactChoice1">Сравнение двух интеллект карт с референцными</label>
-        <p>
-        <input type=radio id="contactChoice2" name="form_algo" value="with_ref">
-            <label for="contactChoice2">Сравнение карт с референцными</label>
-        <p>
-        <label for="file">Загрузка файла с входными данными</label>
-        <p>
-        <input type=file name=file_input>
-        <input type=submit value=Загрузить>
-        <p>
-        <button>Скачать файл с результатами</button>
-    </form>
-    '''
+    return render_template('index.html')
 
 
 def take_mm(filename, status = None):
@@ -69,32 +56,49 @@ def take_mm(filename, status = None):
         arr = []
         arr.append(information_for_algo(id_diagram_1))
         arr.append(information_for_algo(id_diagram_2))
+
         arr_ids = gf.read_mm_ids(sheet, "B")
         n = len(arr_ids)
         mass = [[0] * 2 for i in range(n)]
-
         for i in range(0, len(arr_ids)):
             for j in range(0, 2):
+                curr_map = information_for_algo(arr_ids[i])
+                mass[i][j] = si.max_common_substree_rooted(arr[j]['diagram'], curr_map['diagram'])
+                '''
                 curr_map = information_for_algo(arr_ids[i])
                 new_graph = copy.deepcopy(arr[j]['graph'])
                 algo = sub_algo.max_comp_element(arr[j]['diagram'], new_graph, curr_map['graph'])
                 last_graph = gf.create_graph_form_list(algo)
                 mass[i][j] = af.similarity_sub_algo(last_graph, arr[j]['graph'], curr_map['graph'])
+                '''
+        shod = si.max_common_substree_rooted(arr[0]['diagram'], arr[1]['diagram'])
         output_sheet = wb.create_sheet('matrix')
         gf.print_matrix(output_sheet, mass)
         wb.save(filename)
+        '''
+        new_graph = copy.deepcopy(arr[0]['graph'])
+        algo = sub_algo.max_comp_element(arr[0]['diagram'], new_graph, arr[1]['graph'])
+        last_graph = gf.create_graph_form_list(algo)
+        qweqwe = af.similarity_sub_algo(last_graph, arr[0]['graph'], arr[1]['graph'])
+        '''
+
     else:
         arr_ids = gf.read_mm_ids(sheet, "B")
         n = len(arr_ids)
         mass = [[0] * n for i in range(n)]
         for i in range(0, len(arr_ids)):
             for j in range(0, len(arr_ids)):
+                mind_map_1 = coggle_user.diagram(arr_ids[i])
+                mind_map_2 = coggle_user.diagram(arr_ids[j])
+                mass[i][j] = si.max_common_substree_rooted(mind_map_1, mind_map_2)
+                '''
                 mind_map_1 = information_for_algo(arr_ids[i])
                 mind_map_2 = information_for_algo(arr_ids[j])
                 new_graph_1 = copy.deepcopy(mind_map_1['graph'])
                 algo = sub_algo.max_comp_element(mind_map_1['diagram'], new_graph_1, mind_map_2['graph'])
                 last_graph = gf.create_graph_form_list(algo)
                 mass[i][j] = af.similarity_sub_algo(last_graph, mind_map_1['graph'], mind_map_2['graph'])
+                '''
         output_sheet = wb.create_sheet('matrix')
         gf.print_matrix(output_sheet, mass)
         wb.save(filename)
